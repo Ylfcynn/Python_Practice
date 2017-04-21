f61c2b4adb957d304c516e35a4a714847a964210
Ylfcynn <dobyfinn@gmail.com>
2017-04-21 12:33:00 -0700

    Lunch-time push of saved work.

--- credit.py
+++ credit.py
@@ -1,13 +1,61 @@
 """
+Goal
 
->>> validate([4, 5, 5, 6, 7, 3, 7, 5, 8, 6, 8, 9, 9, 8, 5, 5])
-Valid!
+Write a function which returns whether a string containing a credit card number is valid as a boolean.
+Write as many sub-functions as necessary to solve the problem.
+Write doctests for each function.
 
->>> validate([6, 5, 1, 6, 4, 3, 7, 5, 1, 6, 4, 9, 3, 8, 5, 4])
-Invalid!
+--------------------
 
+Instructions
+
+1. Slice off the last digit. That is the **check digit**.
+2. Reverse the digits.
+3. Double every other element in the reversed list.
+4. Subtract nine from numbers over nine.
+5. Sum all values.
+6. Take the second digit of that sum.
+7. If that matches the check digit, the whole card number is valid.
+
+For example, the worked out steps would be:
+
+1. `4  5  5  6  7  3  7  5  8  6  8  9  9  8  5  5`
+2. `4  5  5  6  7  3  7  5  8  6  8  9  9  8  5`
+3. `5  8  9  9  8  6  8  5  7  3  7  6  5  5  4`
+4. `10 8  18 9  16 6  16 5  14 3  14 6  10 5  8`
+5. `1  8  9  9  7  6  7  5  5  3  5  6  1  5  8`
+6. 85
+7. 5
+8. Valid!
 """
 
+
 def validate(account_number):
-   #Write your code here.
+    """
+
+    >>> validate([4, 5, 5, 6, 7, 3, 7, 5, 8, 6, 8, 9, 9, 8, 5, 5])
+    Valid!
+
+    >>> validate([6, 5, 1, 6, 4, 3, 7, 5, 1, 6, 4, 9, 3, 8, 5, 4])
+    Invalid!
+
+    """
+
+    plastic_fantastic = list()  # Preferred over using 'var = []'
+    card_nums = account_number[0:len(account_number) - 1:1]
+
+    card_nums_r = card_nums[::-1]
+
+    for n in range(len(card_nums_r)):
+        elem = card_nums_r[n]
+        if n % 2 == 0:
+            elemite = elem * 2
+            print(elemite)
+        else:
+            elemite = elem
+
+        plastic_fantastic.append(elemite)
+
+
+
    return
